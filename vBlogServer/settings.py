f7df4bc99a0b0e97e7fe7dd5af0550ace425930e
vachagancode <140316187+vachagancode@users.noreply.github.com>
2024-06-20 11:01:45 +0400

    Update: COR Policy

--- vBlogServer/settings.py
+++ vBlogServer/settings.py
@@ -71,6 +71,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://vblogapp.vercel.app"
 ]
 
 ROOT_URLCONF = 'vBlogServer.urls'
