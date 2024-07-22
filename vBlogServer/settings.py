69a952a06d139d9776c40e170bfbe98044b74dd7
vachagancode <140316187+vachagancode@users.noreply.github.com>
2024-07-22 21:49:59 +0400

    Update settings.py

--- vBlogServer/settings.py
+++ vBlogServer/settings.py
@@ -145,15 +145,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/images/'
+# STATIC_URL = '/static/'
+# MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-	BASE_DIR / 'static'
-]
+# STATICFILES_DIRS = [
+# 	BASE_DIR / 'static'
+# ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
-STATIC_ROOTS = BASE_DIR / 'staticfiles'
+# MEDIA_ROOT = BASE_DIR / 'static/images'
+# STATIC_ROOTS = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
