56f167d958d08980b5cdf2d3e0da779fb83b1df1
vachagancode <140316187+vachagancode@users.noreply.github.com>
2024-07-23 10:51:22 +0400

    Update settings.py

--- vBlogServer/settings.py
+++ vBlogServer/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "5b7qcwv&yv3x**v#(l-elk=d(@o*6x8d$r1v44a2%bw-4ylifb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
